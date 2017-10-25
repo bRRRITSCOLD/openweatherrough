16d45b466d3a17dbc7e9420e3672c8b8e8c9bf67
bRRRITSCOLD <31289446+bRRRITSCOLD@users.noreply.github.com>
2017-10-24 21:20:24 -0500

    Update current_spec.rb

--- spec/current_spec.rb
+++ spec/current_spec.rb
@@ -33,17 +33,22 @@ describe Current do
     	end
   	end
 
-  	describe '#retrieve' do
+	  	describe '#current_temperature_retrieval' do
 	  	it 'successfully retrieves temperature from api data request' do
+	  			json_a = %Q[{"coord":{"lon":-92.64,"lat":41.3},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":8.63,"pressure":1015,"humidity":45,"temp_min":8,"temp_max":10},"visibility":16093,"wind":{"speed":7.2,"deg":320,"gust":9.8},"clouds":{"all":1},"dt":1508888100,"sys":{"type":1,"id":892,"message":0.1682,"country":"US","sunrise":1508934840,"sunset":1508973271},"id":4870239,"name":"Oskaloosa","cod":200}]
 	  			temp = Current.new
-	  			temp.city = 'Chicago'
-	  			temp.country = 'IL'
-	  			temp_response = temp.retrieve
+	  			temp.city = 'Oskaloosa'
+	  			temp.country = 'US'
+	  			json_b = JSON.parse(json_a)
+	  			json_c = json_b['main']
+	  			json_d = json_c['temp']
+	  			temp.temperature = json_d.to_f
 
-	  			expect(temp_response).to be_an_instance_of(Float)
+	  			expect(temp.temperature).to be_an_instance_of(Float)
 	  	end
 	end
 
+end
 end
 
 	
