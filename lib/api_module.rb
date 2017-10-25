b07d6b1842eb5a8020d362f5ea6c9a95e204ed59
bRRRITSCOLD <31289446+bRRRITSCOLD@users.noreply.github.com>
2017-10-24 21:04:24 -0500

    Create api_module.rb

---
+++ lib/api_module.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+
+module API_KEY
+
+	def api_key_loader
+		if File.exist?(File.expand_path('../api.yml', __FILE__))
+			ENV.update YAML.load(File.read(File.expand_path('../api.yml', __FILE__)))
+		else
+			ENV.update YAML.load(File.read(File.expand_path('../test.yml', __FILE__)))
+		end
+	end
+end
