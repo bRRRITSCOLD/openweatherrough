4a11a26dbc46857d56ba80591639e2b5aa7794b0
Blaine Richardson <blainerichardson@Blaines-MacBook-Pro.local>
2017-10-24 20:24:50 -0500

    spec tidying

--- spec/current_spec.rb
+++ spec/current_spec.rb
@@ -38,7 +38,7 @@ describe Current do
 
   	end
 
-  	describe '#retrieve' do
+  	describe '#current_temperature_retrieval' do
 	  	it 'successfully retrieves temperature from api data request' do
 	  			json_a = %Q[{"coord":{"lon":-92.64,"lat":41.3},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":8.63,"pressure":1015,"humidity":45,"temp_min":8,"temp_max":10},"visibility":16093,"wind":{"speed":7.2,"deg":320,"gust":9.8},"clouds":{"all":1},"dt":1508888100,"sys":{"type":1,"id":892,"message":0.1682,"country":"US","sunrise":1508934840,"sunset":1508973271},"id":4870239,"name":"Oskaloosa","cod":200}]
 	  			temp = Current.new
