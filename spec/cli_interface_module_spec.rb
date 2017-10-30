655bd4d08c74d8c3d52e6a42b92ad97aea3349b4
Blaine Richardson <blainerichardson@Blaines-MacBook-Pro.local>
2017-10-30 09:20:18 -0500

    new specs

--- spec/cli_interface_module_spec.rb
+++ spec/cli_interface_module_spec.rb
@@ -19,10 +19,11 @@ describe CLI do
 		# \n
 
 			dummy = Current.new
+
 			expect{dummy.welcome_message}.to output("\n------------------------------------\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n____________________________________\n\nWelcome to the Open Weather Map CLI\n\n------------------------------------\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n____________________________________\n").to_stdout
+		
 		end
 	end
-
 
 	describe '#current_temperature_message' do
 		it "Current_temperature_message puts (displays) via CLI to user" do
@@ -34,7 +35,9 @@ describe CLI do
 		# \n
 
 			dummy = Current.new
+
 			expect{dummy.current_temperature_message}.to output("\n\n_________________________________________________\nThe current temperature in #{@city_name}, #{@country_state_input}: #{@temperature}.\n_________________________________________________\n").to_stdout
+		
 		end
 	end
 
@@ -54,6 +57,7 @@ describe CLI do
 			expect(io_obj)
 	        	.to receive(:strip)
 	        	.and_return(:country.upcase)
+	    	
 	    	end
 
    		it 'City_input and country_state_input accordingly to user inputs, also capitalizes/upcases inputs' do
@@ -76,6 +80,7 @@ describe CLI do
 
     		expect(dummy.instance_variable_get(:@city_input)).to eq(:city.to_s.capitalize)
       		expect(dummy.instance_variable_get(:@country_state_input)).to eq(:country.to_s.upcase)
+	    	
 	    	end	
 	end
 
@@ -91,7 +96,9 @@ describe CLI do
 			
 			dummy = Current.new
 			dummy.ex_yes_no_invalid_message
-			allow($stdout).to receive(:puts)
+
+			expect{dummy.ex_yes_no_invalid_message}.to output("\n\n_________________________________________________________________\nYour input of 'no' was not recognized. Please try again.\n_________________________________________________________________\n").to_stdout
+		
 		end
 	end
 
@@ -103,10 +110,12 @@ describe CLI do
 		# \n'#{@city_input}, #{@country_state_input}' is not a valid combination. Please try again.
 		# \n_________________________________________________________________________________
 		# \n	
-		
+
 			dummy = Current.new
 			dummy.city_country_state_inputs_invalid_message
-			allow($stdout).to receive(:puts)
+			
+			expect{dummy.city_country_state_inputs_invalid_message}.to output("\n\n_________________________________________________________________________________\n'#{@city_input}, #{@country_state_input}' is not a valid combination. Please try again.\n_________________________________________________________________________________\n").to_stdout
+		
 		end
 	end
 end
