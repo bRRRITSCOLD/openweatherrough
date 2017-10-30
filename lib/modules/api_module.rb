5a25ca31a6dd93a11ff83bbe91eaf16d9965e6ad
Blaine Richardson <blainerichardson@Blaines-MacBook-Pro.local>
2017-10-30 08:42:40 -0500

    spec

--- lib/modules/api_module.rb
+++ lib/modules/api_module.rb
@@ -3,7 +3,9 @@ require 'yaml'
 module API_KEY
 
 	def api_key_loader
-		if File.exist?(File.expand_path('../api.yml', __FILE__))
+		if File.exist?(File.expand_path('../api_key.yml', __FILE__))
+			ENV.update YAML.load(File.read(File.expand_path('../api_key.yml', __FILE__)))
+		elsif File.exist?(File.expand_path('../api.yml', __FILE__))
 			ENV.update YAML.load(File.read(File.expand_path('../api.yml', __FILE__)))
 		else
 			ENV.update YAML.load(File.read(File.expand_path('../test.yml', __FILE__)))
